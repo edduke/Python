8f5229edd5cfaabc7b066ea81e073780e649a6c8
Tech Papi <31687633+edduke@users.noreply.github.com>
2023-03-12 23:36:02 +0100

    Update EDA.py

--- EDA.py
+++ EDA.py
@@ -1,5 +1,8 @@
 import pandas as pd 
 import numpy as np 
 import seaborn as sns 
+import matplotlib.pyplot as plt 
+sns.set_theme(color_codes = True) 
 
-// first import the necessary libraries for examining your data 
+
+--- first import the necessary libraries for examining your data 
